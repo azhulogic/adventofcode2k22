5a65be100d28cc3caea3a9d20514f8e37a78903c
azhulogic <43792633+azhulogic@users.noreply.github.com>
2022-12-02 14:21:31 -0600

    Update day2.py

--- day2/day2.py
+++ day2/day2.py
@@ -4,7 +4,7 @@
 
 def rock_paper_scissors(fileName):
     """Do you dare try to figure out what I wrote? O(n)"""
-    return sum(list(map(lambda s : (ord(s[2])-ord(s[0])+2) % 3 * 3 + (ord(s[2]) + 1) % 4, open(fileName))))
+    return sum(list(map(lambda s : (ord(s[2])-ord(s[0])+2) % 3 * 3 + (ord(s[2])+1) % 4, open(fileName))))
 
 def scissors_paper_rock(fileName):
     """Okay, coding like a normal person. Kind of."""
