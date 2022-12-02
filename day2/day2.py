ecf14153ecbf12decea3f252d09c773898f5005d
Alex <zhux0533@umn.edu>
2022-12-02 14:10:08 -0600

    day2 part2

--- day2/day2.py
+++ day2/day2.py
@@ -5,10 +5,22 @@
 
 def rock_paper_scissors(fileName):
     """Do you dare try to figure out what I wrote? O(n)"""
-    score = [3,0,6]
     with open(fileName) as f:
-        return sum(list(map(lambda s: score[(((ord(s[0]) - 61) - (ord(s[2]) - 87)) % 3)] + (ord(s[2]) - 87), f)))
+        return sum(list(map(lambda s : (ord(s[2])-ord(s[0])+2) % 3 * 3 + (ord(s[2]) + 1) % 4, f)))
 
+def scissors_paper_rock(fileName):
+    """Okay, coding like a normal person. Kind of."""
+    score = 0
+    with open(fileName) as f:
+        for l in f:
+            elf_play = ord(l[0]) - 65 # {0,1,2} : rock paper scissors
+            you_play = ord(l[2]) - 88 # {0,1,2} : lose, draw, win
+
+            score += (elf_play + you_play - 1) % 3 + 1 + you_play * 3
+    return score
 
 if __name__ == "__main__":
-    print(rock_paper_scissors("day2/example.txt"))
+    print("Example P1 | %d" % rock_paper_scissors("day2/example.txt"))
+    print("Input P1   | %d" % rock_paper_scissors("day2/input.txt"))
+    print("Example P2 | %d" % scissors_paper_rock("day2/example.txt"))
+    print("Input P2   | %d" % scissors_paper_rock("day2/input.txt"))
